f38594a9d34679e42fbe68f2fd67e261cc53582d
JJPEOPLES <primestock91@gmail.com>
2025-07-20 21:50:09 -0400

    Initial commit

--- netlify-build.sh
+++ netlify-build.sh
@@ -4,10 +4,15 @@ set -e
 # Set environment variables
 export CI=false
 export TSC_COMPILE_ON_ERROR=true
+export NODE_OPTIONS=--max_old_space_size=4096
 
 # Install GT4 dependencies with legacy-peer-deps
 echo "Installing GT4 dependencies..."
 npm install --legacy-peer-deps
+
+# Ensure react-scripts is installed globally
+echo "Installing react-scripts globally..."
+npm install -g react-scripts
 
 # Build GT4
 echo "Building GT4..."
@@ -18,6 +23,13 @@ echo "Building GT5..."
 cd GT5
 echo "Installing GT5 dependencies..."
 npm install --legacy-peer-deps
+
+# Ensure react-scripts is available in GT5
+echo "Ensuring react-scripts is available in GT5..."
+npm link react-scripts
+
+# Build GT5
+echo "Building GT5..."
 npm run build
 cd ..
 
